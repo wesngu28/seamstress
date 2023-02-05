57a3d8769df1e58e815bfa55161dc804df6f24c0
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-02-05 01:05:54 -0800

    fix current user + rm console log

--- src/routes/scissors/fetchUrls.js
+++ src/routes/scissors/fetchUrls.js
@@ -1,12 +1,12 @@
 import { getDocs, query, where } from "firebase/firestore/lite";
-import { linksRef } from "../../util/firebase";
+import { auth, linksRef } from "../../util/firebase";
 
 export async function fetchUrls() {
-		const findLink = query(linksRef, where('creator', '==', usa));
+		const user = auth.currentUser
+		const findLink = query(linksRef, where('creator', '==', user.uid));
 		const querySnapshot = await getDocs(findLink);
 		const userLinks = []
 		querySnapshot.forEach((link) => {
-			console.log(link.data())
 			userLinks.push(link.data())
 		});
 		return userLinks;
